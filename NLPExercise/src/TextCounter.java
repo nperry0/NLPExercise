342e6b25a4f6cd690eb5c13fb1638b41cae29db4
Nick Perry <np0022@mix.wvu.edu>
2019-05-25 16:39:31 -0400

    made function for file-to-text, added word regex

--- NLPExercise/src/TextCounter.java
+++ NLPExercise/src/TextCounter.java
@@ -7,33 +7,47 @@ public class TextCounter {
 
 	public static void main(String[] args) throws FileNotFoundException {
 		File file = new File("/Users/nicholasperry/Desktop/sampletext.txt");
-		Scanner scanner = new Scanner(file);
-		String text = "";
-		while(scanner.hasNextLine()) {
-			text = text + scanner.nextLine() + "\n";
-		}
-		scanner.close();
+		//Don't forget to change input to CLI
+		
+		String text = fileToString(file);
 
 		int words = countWords(text);
 		int sentences = countSentences(text);
 		int paragraphs = countParagraphs(text);
 		
-		//However, words here are defined as anything between the whitespace. Refine.
 		System.out.println("There are " + words + " words.");	
-		//Think about including other punctuation.
 		System.out.println("There are " + sentences + " sentences.");	
 		//This only works if there is a new line at the end of last paragraph
+		//You're adding a new line as part of the ingestion process jackass
 		System.out.println("There are " + paragraphs + " paragraphs.");
 		
 	}
 	
+	public static String fileToString(File file) throws FileNotFoundException {
+		String text = "";
+		Scanner scanner = new Scanner(file);
+		while(scanner.hasNextLine()) {
+			text = text + scanner.nextLine() + "\n";
+		}
+		scanner.close();
+		return text;
+	}
+	
 	public static int countWords(String text) {
-		return text.split("\\s").length;
+		int words = 0;
+		Pattern pattern = Pattern.compile("[[[A-Z]{1}[//.]{1}]{2,}]*[a-zA-Z]+[-']?[a-z]*");
+		// Try to fix missing dangling period from acronym
+		Matcher matcher = pattern.matcher(text);
+		while (matcher.find()) {
+			System.out.println(matcher.toString());
+			words++;
+		}
+		return words;
 	}
 	
 	public static int countSentences(String text) {
 		int sentences = 0;
-		Pattern pattern = Pattern.compile("[a-zA-Z]*[\\.\\-][\\\"]?[\\s]");
+		Pattern pattern = Pattern.compile("[a-zA-Z]*[\\.\\-!\\?][\\\"]?[\\s]");
 		Matcher matcher = pattern.matcher(text);
 		while (matcher.find()) {
 			sentences++;
